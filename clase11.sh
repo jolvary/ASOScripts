82f6e22a94cb5b295497fa43698a9eb7ab6688f4
Jolvary98 <32238907+jolvary@users.noreply.github.com>
2022-02-23 21:58:41 +0100

    Clase 11

--- clase11.sh
+++ clase11.sh
@@ -1,7 +1,11 @@
 read -n 7 -p "Introduce dos números de 3 dígitos cada uno: " num1 num2
 
-for i in num1 num2
+for i in $num1 $num2
 do
 reverso=$(echo $i | rev);
-echo $reverso
+if [[ $i -eq $reverso ]]
+  then
+    echo "El numero es capicua"
+  else
+    echo "El numero no es capicua"
 done
