663fd02fd56d34d8777a309b14d7f52382f2e387
jolvary <jolvary@gmail.com>
2022-02-24 12:15:18 +0100

    Clase36

--- clase36.sh
+++ clase36.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 cont=0
-
+let "cont2=cont+1"
 while [ $cont -lt $# ]
 do
     if [[ $(($1 % 2)) -eq 0 ]]
     then
         echo "pene"
+        ((cont++))
     else
         echo "patata"
+        ((cont++))
     fi
 done
