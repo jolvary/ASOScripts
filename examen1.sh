09b7b4db3d03850847c43614c46cf8ef06b51797
jolvary <jolvary@gmail.com>
2022-02-24 12:35:22 +0100

    examen1

--- examen1.sh
+++ examen1.sh
@@ -4,4 +4,15 @@ read -p "Qué desea realizar :
     1. GID
     2. UID
     3. Salir
+
+Opción: 
     " opcion
+
+case in $opcion
+    1)
+    echo "opcion 1";;
+    2)
+    echo "opcion2 ";;
+    3)
+    echo "opcion 3";;
+esac
