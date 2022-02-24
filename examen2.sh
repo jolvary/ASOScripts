c2ffe42b53114ab845292b8d00f7b2040ed283f6
jolvary <jolvary@gmail.com>
2022-02-24 13:45:34 +0100

    examen2

--- examen2.sh
+++ examen2.sh
@@ -4,7 +4,16 @@ if [ $# -eq 2 ]
 then
     if [[ $(id -nu $1) ]] && [[ $(id -nu $2) ]]
     then
-        echo "Existe"
+        if [[ $1 -gt $2 ]]
+        then
+            primero=$(grep -n :$2: /etc/passwd | cut -f1 -d':')
+            segundo=$(grep -n :$1: /etc/passwd | cut -f1 -d':')
+            echo "La cantidad de líneas entre ambos es de: " $((primero - segundo))
+        else
+            primero=$(grep -n :$1: /etc/passwd | cut -f1 -d':')
+            segundo=$(grep -n :$2: /etc/passwd | cut -f1 -d':')
+            echo "La cantidad de líneas entre ambos es de: " $((primero - segundo))
+        fi
     else
         echo "No existe uno o ambos UIDs"
     fi
